a566e14417b996135c11c08e8534ae9908756ac5
anthony-63 <back.up.email.158@gmail.com>
2023-05-29 19:27:33 -0400

    Add arrows to swipe notes

--- scripts/game/Notes/Swipe.gd
+++ scripts/game/Notes/Swipe.gd
@@ -1,5 +1,12 @@
 extends NoteBase
 
+func init_based_on_type():
+	match note_data.direction:
+		OpusTypes.NoteDirection.LEFT: $Arrow.rotation_degrees.z = 90.0
+		OpusTypes.NoteDirection.DOWN: $Arrow.rotation_degrees.z = 180.0
+		OpusTypes.NoteDirection.RIGHT: $Arrow.rotation_degrees.z = -90.0
+		OpusTypes.NoteDirection.UP: $Arrow.rotation_degrees.z = 0.0
+
 func check_hit():
-	# Check hit for normal note
 	pass
+
